67da2aab4674d5e271e0b0c5aec6f8087e18d7f9
Leticia.Lopes <leticia.rosedesantana@gmail.com>
2023-09-05 00:04:56 -0300

    Adicionado configurações produtor

--- src/main/java/io/github/leticiarose/paymentservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/io/github/leticiarose/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,27 @@ package io.github.leticiarose.paymentservice.service.impl;
 
 import io.github.leticiarose.paymentservice.model.Payment;
 import io.github.leticiarose.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Pagamento Recebido {}", payment);
+        log.info("Pagamento Recebido {}", payment);
+        Thread.sleep(1000);
+        log.info("Enviando pagamento ...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
