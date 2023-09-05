458dc5acd1301d57f2e16387ded086616b1e1619
Leticia.Lopes <leticia.rosedesantana@gmail.com>
2023-09-05 01:15:08 -0300

    Alteracao no resource

--- src/main/java/io/github/leticiarose/paymentservice/resources/impl/PaymentResourceImpl.java
+++ src/main/java/io/github/leticiarose/paymentservice/resources/impl/PaymentResourceImpl.java
@@ -4,7 +4,6 @@ import io.github.leticiarose.paymentservice.model.Payment;
 import io.github.leticiarose.paymentservice.resources.PaymentResource;
 import io.github.leticiarose.paymentservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
